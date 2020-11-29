b544121137a6a6c72b01ca060b1989e5ddff28e3
VINICIUS ALMEIDA DE SOUZA <v.almsou@uol.com.br>
2020-11-29 19:50:01 -0300

    troca de máquina-teste

---
+++ Projeto-Resilia-Modulo-01-main/js/controllers/CadastroController.js
@@ -0,0 +1,26 @@
+class CadastroController {
+    
+    constructor(){
+
+    }
+
+    autoComplete() {
+        
+        let cep = document.querySelector("#inputZip").value;
+        
+        let cadastroModel = new CadastroModel();
+        cadastroModel.buscar(cep);
+        
+        let cadastroView = new CadastroView();
+
+        cadastroView.addEndereco(cadastroModel.endereco);
+        cadastroView.addBairro(cadastroModel.bairro);
+        cadastroView.addCidade(cadastroModel.cidade);
+        cadastroView.addUf(cadastroModel.uf);
+    }
+}
+
+
+
+
+let cadastroController = new CadastroController();
